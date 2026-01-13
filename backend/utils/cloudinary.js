1c0190884fdaa86dec4009def6388e1b88ba87b2
Rajannya214 <nandyrajannya@gmail.com>
2026-01-13 10:29:13 +0530

    code changed

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,12 +1,10 @@
-const uploadToCloudinary = (buffer) => {
-  return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      { folder: "companies" },
-      (error, result) => {
-        if (error) reject(error);
-        else resolve(result);
-      }
-    );
-    stream.end(buffer);
-  });
-};
+import {v2 as cloudinary} from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config();
+
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET
+});
+export default cloudinary;
